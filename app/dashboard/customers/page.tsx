e8230ec7a2f32cfbc858055fa95530f35c451e8f
mia <miaomiaojiaoju@outlook.com>
2025-12-02 14:29:05 +0800

    chapter 5.

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,10 @@
+import {lusitana} from "@/app/ui/fonts";
+
+
+export default function DashboardPage() {
+    return (
+        <p className={lusitana.className}>
+            Customer Page
+        </p>
+    )
+}
